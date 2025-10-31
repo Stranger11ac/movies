ae5df5f2d6b436f203af460cb29a8895062f2388
salvadorhernandezutc <salvadorh@utc.edu.mx>
2025-10-30 19:32:46 -0600

    Initialize again

---
+++ components.php
@@ -0,0 +1,16 @@
+<?php
+    function headContent($title) {
+        echo '<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>'.$title.' | Peliculas</title>
+        
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+        <!-- MDB -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.2.0/mdb.min.css" rel="stylesheet" />
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>';
+    }
+?>
--- index.php
+++ index.php
@@ -1,54 +1,13 @@
-<?php include 'componentes.php'; ?>
+<?php
+    include 'components.php';
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es" data-mdb-theme="light">
     <head>
-        <?php
-            $title = "Inicio";
-            headContent($title);
-        ?>
+        <?php headContent("Inicio de Sesion"); ?>
     </head>
     <body>
-        <section class="d-grid">
-            <form class="card" id="formInsertMovies">
-                <div class="card-header">
-                    <h5 class="card-title text-center">Agregar Peliculas</h5>
-                </div>
-                <div class="card-body">
-                    <!-- formulario de registrar peliculas -out-->
-                    <div data-mdb-input-init class="form-outline mb-4">
-                        <input type="text" id="nameMovie" name="nameMovie" value="Movie name 1" class="form-control" />
-                        <label class="form-label" for="nameMovie">Nombre:</label>
-                    </div>
-                    <div data-mdb-input-init class="form-outline mb-4">
-                        <input type="text" id="nameAuthor" name="nameAuthor" value="Author movie 1" class="form-control" />
-                        <label class="form-label" for="nameAuthor">Director:</label>
-                    </div>
-                    <div data-mdb-input-init class="form-outline mb-4">
-                        <input type="text" id="timeMovie" name="timeMovie" value="120" class="form-control" />
-                        <label class="form-label" for="timeMovie">Duracion:</label>
-                    </div>
-                    <div data-mdb-input-init class="form-outline mb-4">
-                        <input type="text" id="genderMovie" name="genderMovie" value="Action" class="form-control" />
-                        <label class="form-label" for="genderMovie">Genero:</label>
-                    </div>
-                    <div data-mdb-input-init class="form-outline mb-4">
-                        <input type="datetime-local" id="dateMovie" name="dateMovie" value="2025-10-22" min="2025-10-20" class="form-control" />
-                        <label class="form-label" for="dateMovie">FechaLanzamiento:</label>
-                    </div>
-                    <div class="form-outline">
-                        <select name="clasificacionId" id="clasificacionId" class="form-control">
-                            <option value="Selecciona la Clasificacion" disabled selected hidden>Selecciona la Clasificacion</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <!-- Submit button -->
-                    <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block">Sign in</button>
-                </div>
-            </form>
-        </section>
 
-        <?php scriptContent() ?>
     </body>
 </html>
